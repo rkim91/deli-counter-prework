dcf0635f997a7ed293a612faebd8df316f3a9921
Richard Kim <richeekim@gmail.com>
2018-03-29 22:18:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-def line(katz_deli)
+def line(katz_deli, customer)
   if katz_deli == []
     puts "The line is currently empty."
   end
   if
-    katz_deli.map do |x|
+    
     puts "The line is currently: #{x}."
   end
 end
