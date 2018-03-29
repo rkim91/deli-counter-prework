3f0a6498ef4a3e936a8e425720388566ba235b15
Richard Kim <richeekim@gmail.com>
2018-03-29 23:08:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,16 +9,7 @@ def line(katz_deli)
     puts "The line is currently: #{arr.join(" ")}"
   end
 end
-  #   filled = ""
-  #   for x in 0...katz_deli.length
-  #     if x == katz_deli.length-1
-  #       order += "#{x+1}. #{katz_deli[x].}"
-  #     end
-  #   end
-  #   puts "The line is currently: #{order}."
-  # end
-
 
 def take_a_number(katz_deli, name)
-  katz_deli.push(customer)
+  katz_deli.push(name)
 end
