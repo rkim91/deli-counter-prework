e712c080e740f1ecbfb8b7229daf12c0b9ae5ce1
Richard Kim <richeekim@gmail.com>
2018-03-29 23:03:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   else
     arr = []
     katz_deli.each_with_index do |x, i|
-      arr << "#{i+1}. #{x}."
+      arr << "#{i+1} #{x}."
     end
   end
   puts "The line is currently: #{arr.join(",")}"
