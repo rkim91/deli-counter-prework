381dec5511f848741481d94f44957ebcbe7e9605
Richard Kim <richeekim@gmail.com>
2018-03-29 23:05:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
     arr = []
     katz_deli.each_with_index do |x, i|
       arr << "#{i+1}. #{x}"
-    puts "The line is currently: #{arr.join(" ")}"
     end
+    puts "The line is currently: #{arr.join(" ")}"
   end
 end
   #   filled = ""
