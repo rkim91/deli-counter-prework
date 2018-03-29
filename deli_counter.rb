a8b63457ed4a427922b137f489b28d618ecff8d1
Richard Kim <richeekim@gmail.com>
2018-03-29 22:34:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif
-    # for x in 0...katz_deli.length
+  end
+  if
+    filled = ""
+    for x in 0...katz_deli.length
       puts "The line is currently: #{katz_deli}."
     end
   end
